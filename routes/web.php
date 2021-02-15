b0dbd6cb7571906e364d11bb4986ad77d59c6b8a
alaa mohamed <ala96ala96@gmail.com>
2021-02-15 16:35:33 +0200

    edit mien root

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,14 @@ use App\Http\Controllers\Dasboard\SystemController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome2');
-});
+// Route::get('/', function () {
+//     return view('welcome2');
+// });
 // Route::get('/login', function () {
 //     return view('login');
 // });
 // Route::get('dashboard/', [DashboardController::class,'index']);
 Route::post('login/', [SystemController::class,'login'])->name('login');
-Route::get('/login', Login::class);
+Route::get('/', Login::class);
 Route::get('/dashboard', App\Http\Livewire\Index::class);
 Route::get('/my-oreders', MyOrders::class);
