d6f6d85c1c097d36b3fd00084a5e58f0dd039d9d
Alaa M Shaban <ala96ala96@gmail.com>
2023-02-24 20:10:32 +0200

    delete user with delete token

--- app/Http/Livewire/Dashboard/User/Index.php
+++ app/Http/Livewire/Dashboard/User/Index.php
@@ -5,26 +5,28 @@ namespace App\Http\Livewire\Dashboard\User;
 use App\Models\User;
 use Livewire\Component;
 use Livewire\WithPagination;
-use Illuminate\Support\Facades\Session;
 
 class Index extends Component
 {
-    use WithPagination ;
+    use WithPagination;
     public function render()
     {
-        $users=User::where('status',1)->where('pharmacy_id',null)->paginate(10);
-        return view('livewire.dashboard.user.index',compact('users'));
+        $users = User::where('status', 1)->where('pharmacy_id', null)->paginate(10);
+        return view('livewire.dashboard.user.index', compact('users'));
     }
     public function destroy(User $user)
     {
         try {
-            $user->status=3;
+            $user->status = 3;
+            $user->tokens->each(function ($token, $key) {
+                $token->delete();
+            });
             $user->save();
-            $this->dispatchBrowserEvent('success-tost',['action'=>"الحذف"]);
+            $this->dispatchBrowserEvent('success-tost', ['action' => "الحذف"]);
 
-        } catch (\Throwable $th) {
-            $this->dispatchBrowserEvent('error-tost',['action'=>"الحذف"]);
+        } catch (\Throwable$th) {
+            $this->dispatchBrowserEvent('error-tost', ['action' => "الحذف"]);
         }
-       
+
     }
-} 
+}
