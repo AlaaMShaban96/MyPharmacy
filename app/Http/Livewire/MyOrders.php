204df04d562f8bfecccdd4b592dbe82c31d6f20f
Alaa M Shaban <ala96ala96@gmail.com>
2023-02-24 23:00:37 +0200

    edit order by

--- app/Http/Livewire/MyOrders.php
+++ app/Http/Livewire/MyOrders.php
@@ -2,45 +2,44 @@
 
 namespace App\Http\Livewire;
 
+use App\Events\SendNotification;
 use App\Models\Order;
 use Livewire\Component;
-
 use Livewire\WithPagination;
-use App\Events\SendNotification;
-
 
 class MyOrders extends Component
 {
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
-    private  $orders;
-    public $order = ['price','text' ];
+    private $orders;
+    public $order = ['price', 'text'];
     public function render()
     {
-            $this->orders=auth()
+        $this->orders = auth()
             ->user()
             ->pharmacy
             ->orders()
-            ->where('public',0)
-            ->wherePivot('status',1)
+            ->where('public', 0)
+            ->orderBy('id', 'DESC')
+            ->wherePivot('status', 1)
             ->get();
-   
-        return view('livewire.my-orders',['orders'=>$this->orders]);
+
+        return view('livewire.my-orders', ['orders' => $this->orders]);
     }
-    public function replayOrder(Order $order,$key)
+    public function replayOrder(Order $order, $key)
     {
-        
-       auth()
-       ->user()
-       ->pharmacy
-       ->orders()
-       ->updateExistingPivot($order->id,[
-            'price'=>$this->order[$key]['price'],
-            'text'=>$this->order[$key]['text'],
-            'status'=>2
-        ]);
-        $notification="تم الرد على الطلب من قبل ".auth()->user()->pharmacy->name;
-        event(new SendNotification($order->user,$notification));
-        $this->dispatchBrowserEvent('success-tost',['action'=>"الرد على الطلب "]);
+
+        auth()
+            ->user()
+            ->pharmacy
+            ->orders()
+            ->updateExistingPivot($order->id, [
+                'price' => $this->order[$key]['price'],
+                'text' => $this->order[$key]['text'],
+                'status' => 2,
+            ]);
+        $notification = "تم الرد على الطلب من قبل " . auth()->user()->pharmacy->name;
+        event(new SendNotification($order->user, $notification));
+        $this->dispatchBrowserEvent('success-tost', ['action' => "الرد على الطلب "]);
     }
 }
